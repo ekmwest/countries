ce975b4fef8952adddc9c51e442add0106f4cdc6
Karl West <mickel.west@gmail.com>
2022-07-27 14:07:03 +0200

    upgrade piko and deno std

--- deps.js
+++ deps.js
@@ -1,3 +1,3 @@
-export * as path from "https://deno.land/std@0.140.0/path/mod.ts";
+export * as path from "https://deno.land/std@0.149.0/path/mod.ts";
 export { DB } from "https://deno.land/x/sqlite@v3.4.0/mod.ts";
-export { dev } from "https://cdn.jsdelivr.net/gh/learnpoint/piko@1.0.7/dev.js";
+export { dev } from "https://cdn.jsdelivr.net/gh/learnpoint/piko@1.0.14/dev.js";
